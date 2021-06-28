c94f9bcad75710d6a650a0deeaec5bdb808c51e2
Hassan998-cloud <81471046+Hassan998-cloud@users.noreply.github.com>
2021-06-27 21:55:27 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -13,21 +13,29 @@ recognition.onresult = function(event){
 
     document.getElementById("textbox").innerHTML = Content;
     console.log(Content);
- 
-    speak();
+      if(Content == "take my selfie"){
+          speak();
+    }
+
 
 }
 
 function speak(){
     var synth = window.speechSynthesis;
 
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "Taking my selfie in 5 seconds";
 
     var utterThis = new SpeechSynthesisUtterance(speak_data);
 
     synth.speak(utterThis);
 
     Webcam.attach(camera);
+   setTimeout(function()
+   {
+        take_snapshot();
+        save();
+        
+   }, 5000);
 }
 
 camera = document.getElementById("camera");
@@ -37,3 +45,18 @@ Webcam.set({
     image_format: 'png',
     png_quality:90
 });
+
+function take_snapshot(){
+  Webcam.snap(function(data_uri){
+      document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
+  });
+    
+}
+
+function save(){
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
+
